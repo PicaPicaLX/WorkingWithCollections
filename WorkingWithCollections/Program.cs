e51f421235e863857eb2601d4c3156fcdf7e8e24
栗鑫 <lx389029980@outlook.com>
2018-02-09 21:47:19 +0800

    V1

--- WorkingWithCollections/Program.cs
+++ WorkingWithCollections/Program.cs
@@ -9,7 +9,32 @@ namespace WorkingWithCollections
     class Program
     {
         static void Main(string[] args)
-        {        
+        {
+            Car car1 = new Car();
+            car1.Make = "Oldmobile";
+            car1.Model = "Cutlas Supreme";
+
+            Car car2 = new Car();
+            car2.Make = "Geo";
+            car2.Model = "Prism";
+
+            Book book1 = new Book();
+            book1.Aythor = "Robert Tabor";
+            book1.Title = "Microsoft .NET XML Web Services";
+            book1.ISBN = "0-000-00000-0";
         }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }  
+        public string Model { get; set; }
+    }
+
+    class Book
+    {
+        public string Title { get; set; }
+        public string Aythor { get; set; }
+        public string ISBN { get; set; }
     }
 }
